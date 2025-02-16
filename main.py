3500ee87f814e9b12237d719d20ce6a7e7f3b04b
Yannick Remy <remyyannick@sfr.fr>
2025-02-16 19:47:41 +0100

    Feat: ajout des statuts des tâches

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ from pymongo import MongoClient
 client = MongoClient("mongodb://localhost:27017/")
 db = client["todolist"]
 collection = db["tasks"]
+
+# Statuts des tâches
+status_labels = {0: "Nouvelle", 1: "En cours", 2: "Terminée"}
