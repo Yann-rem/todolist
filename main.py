c4a7aeedaa7c21e5a6271ad4905da3c2534af7aa
Yannick Remy <remyyannick@sfr.fr>
2025-02-16 19:50:31 +0100

    Feat: ajout d'une fonction pour affichager les tâches

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ collection = db["tasks"]
 
 # Statuts des tâches
 status_labels = {0: "Nouvelle", 1: "En cours", 2: "Terminée"}
+
+
+# Fonction pour afficher les tâches
+def show_tasks():
+    tasks = list(collection.find())
+    if not tasks:
+        print("\nAucune tâche disponible.\n")
+    else:
+        print("\nListe des tâches :")
+        for task in tasks:
+            print(
+                f"ID: {task['_id']} | Description: {task['description']} | Statut: {status_labels[task['status']]}"
+            )
